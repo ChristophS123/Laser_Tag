21b1283b78c70da624995f890053b83fc864a112
Christoph <schmitzchristoph7@gmail.com>
2022-09-05 22:12:54 +0200

    minecraft-standart-plugin

---
+++ src/main/java/de/christoph/lasertag/LaserTag.java
@@ -0,0 +1,11 @@
+package de.christoph.lasertag;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class LaserTag extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("Test");
+    }
+}
